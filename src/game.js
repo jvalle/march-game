eda94c21c870a01a9cd1745bdfb9686917e56983
jason <jcvalle@mail.usf.edu>
2015-03-18 10:39:58 -0600

    scaffold out some game entry points

---
+++ src/game.js
@@ -0,0 +1,14 @@
+import Entity from './Entity';
+
+var Game = {
+	init: function () {
+		// init game
+		// set canvas el
+		// set ctx
+		// initialize entities
+	}
+};
+
+window.Game = Game;
+
+export default Game;
