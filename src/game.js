ca705fe883758685146e4f7a662f22d405381813
jason <jcvalle@mail.usf.edu>
2015-03-18 15:09:55 -0600

    add canvas and call game.init on domcontentready

--- src/game.js
+++ src/game.js
@@ -1,12 +1,20 @@
 import Entity from './Entity';
 
 var Game = {
-	init: function () {
-		// init game
-		// set canvas el
-		// set ctx
-		// initialize entities
-	}
+
+    canvas: null,
+    context: null,
+
+    init: function (canvas) {
+        // set canvas el
+        if (!canvas) {
+            throw new Error('Game must be initialized on a canvas element.');
+        }
+        this.canvas = canvas;
+        // set ctx
+        this.context = canvas.getContext('2d');
+        // initialize entities
+    }
 };
 
 window.Game = Game;
